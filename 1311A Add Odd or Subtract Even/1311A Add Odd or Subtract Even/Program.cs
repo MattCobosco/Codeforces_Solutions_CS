aebbd353b8c6144054c505ae6776cb596dc265e1
MattCobosco <mateuszkalucki@gmail.com>
2021-04-10 00:41:13 +0200

    1311A

---
+++ 1311A Add Odd or Subtract Even/1311A Add Odd or Subtract Even/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace _1311A_Add_Odd_or_Subtract_Even
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int t = Convert.ToInt32(Console.ReadLine());
+
+            for (int i=0; i<t; i++) 
+            {
+                string[] ab = Console.ReadLine().Split();
+
+                int a = int.Parse(ab[0]);
+                int b = int.Parse(ab[1]);
+
+                // I can add an odd int / subtract an even int
+                // dodawać nieparzyste/odejmować parzyste
+                int steps;
+
+                if (a > b)
+                {
+                   if ((a - b) % 2 == 0)
+                   {
+                       steps = 1;
+                   }
+                   else
+                   {
+                       steps = 2;
+                   }
+                }
+                else if (a < b)
+                {
+                    if ((a - b) % 2 == 0)
+                    {
+                        steps = 2;
+                    }
+                    else
+                    {
+                        steps = 1;
+                    }
+                }
+                else
+                {
+                    steps = 0;
+                }
+                Console.WriteLine(steps);
+            }
+        }
+    }
+}
