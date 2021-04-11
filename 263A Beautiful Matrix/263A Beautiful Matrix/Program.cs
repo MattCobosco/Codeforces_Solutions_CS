cc67a848cf4697b8b94f5fda7dd52f8b1d83c896
MattCobosco <mateuszkalucki@gmail.com>
2021-04-12 01:27:46 +0200

    added 263A and 59A

---
+++ 263A Beautiful Matrix/263A Beautiful Matrix/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _263A_Beautiful_Matrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   /* 
+             * The idea is to split the matrix into rows and elements of the row.
+             * In order to do so the 2 for loops are used, first to split the matrix into rows and second to split every row into individual elements.
+             * If the analysed element is 1 then the program assigns i+1 and j+1 to variables row and column respectively (+1 to compensate for 0-based counting in C#)
+             * Then to calculate the distance from the center one can use the sum of 
+             * absolute values of the r3,c3 (center cordinates) minus the cordinates of the "1" element.
+             */
+            int row = 0; 
+            int column=0;
+            for (int i=0; i < 5; i++) // split matrix into rows
+            {
+                string [] inputRow = Console.ReadLine().Split();
+
+                for (int j = 0; j < 5; j++) //split each row into elements and analyse them one by one
+                {
+                    int inputColumn = int.Parse(inputRow[j]);
+                    // if the checked element is 1 then calculate its coordinates and stop the loop 
+                    if (inputColumn == 1)
+                    {
+                        row = i + 1;
+                        column = j + 1;
+                        break;
+                    }
+                }
+            } // calculate the coordinates and print their sum
+            Console.WriteLine(Math.Abs(3 - row) + Math.Abs(3 - column));
+        }
+    }
+}
---
+++ 59A Word/59A Word/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _59A_Word
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine(); // read the line and save as string
+            char[] inputSplit = input.ToCharArray(); // convert the conver the string into a char array
+            bool tempUpper; // bool stating whether the analysed letter is uppercase or not
+            int upper = 0; // counter of the uppercase letters
+            int lower = 0; // counter of the lowercase letters
+
+            for (int i = 0; i < input.Length; i++) // for loop to analyse the letters one by one
+            {
+                char tempLetter = inputSplit[i]; //
+
+                tempUpper = char.IsUpper(tempLetter); // checking if the analysed char is uppercase
+
+
+                if (tempUpper) // if it's uppercase then increase uppercase letter counter by 1
+                    upper++;
+                else
+                    lower++; // if it isn't then increase lowercase letter counter by 1
+            }
+
+            if (upper > lower) // if there's more uppercase letters than lowercase letters print the input string converted to uppercase letters
+                Console.WriteLine(input.ToUpper());
+            else // if there isn't print the input string converted to lowercase letters
+                Console.WriteLine(input.ToLower());
+        }
+    }
+}
