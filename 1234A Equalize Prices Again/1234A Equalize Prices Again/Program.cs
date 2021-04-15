74ee9b22bc9a92927cd975c07b2e82d2e3714160
MattCobosco <mateuszkalucki@gmail.com>
2021-04-15 22:04:26 +0200

    added 1234A

---
+++ 1234A Equalize Prices Again/1234A Equalize Prices Again/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _1234A_Equalize_Prices_Again
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int.TryParse(Console.ReadLine(), out int q); // read q (number of cases) from input
+
+            for (int i = 0; i < q; i++) // loop through q cases
+            {
+                int n = Convert.ToInt32(Console.ReadLine()); // read n (number of prices in a case) from input
+
+                string[] prices = Console.ReadLine().Split(); //read prices as string from input
+
+                decimal pricesSum = 0; //sum of prices is initially 0
+
+                for (int j = 0; j < n; j++) // loop through the prices
+                {
+                    pricesSum += int.Parse(prices[j]); // convert the prices from the string into ints and add them to the sum one by one
+                }
+
+                decimal averagePriceCeiling = Math.Ceiling(pricesSum / n); // calculate the ceiling of the average price
+
+                Console.WriteLine(averagePriceCeiling); // print it as output
+            }
+        }
+    }
+}
