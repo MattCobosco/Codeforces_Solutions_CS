d6272b1c432b9c422357ee4828819711c3f01104
MattCobosco <mateuszkalucki@gmail.com>
2021-07-06 15:13:48 +0200

    Added 732A and 702A

---
+++ 702A Maximum Increase/702A Maximum Increase/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace _702A_Maximum_Increase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine()); // read array lenght from the first line
+
+            string[] input = Console.ReadLine().Split(); // read second line as input string
+
+            /*  The idea is to loop through the input numbers analysing i-th and i+1-th numbers to check if the latter is bigger than the former. As long as it's true the counter of bigger numbers will be increased by 1.
+                As long as that counter is rising, it will be saved as maxCounter. As soon as the i+1-th number is smaller or equal to the i-th number, the counter of bigger numbers will be reset to 1, and the last value of the maxCounter will be saved for further reference. 
+                Then the program will search for a next increasing subarray and compare it's length to the longest */
+
+
+            int bigCounter = 1; // set counter of numbers bigger than predecessor to 1 (always at least the first number will be an increasing array)
+            int maxCounter = bigCounter; // set max of the bigCounter to 1 too
+            for (int i = 0; i < n - 1; i++) // loop through the numbers from input
+            {
+                int x = int.Parse(input[i]); // x is i-th number from input string
+                int y = int.Parse(input[i + 1]); // y is i+1-th number from input string
+
+                if (x < y) // if the i+1-th number is bigger than the i-th number than it's a part of increasing string, therefore increase the bigCounter by 1
+                {
+                    bigCounter++;
+                    
+                    if (bigCounter > maxCounter) // save the bigCounter as maxCounter as long as it's bigger than the previously identified maxCounter
+                    {
+                        maxCounter=bigCounter;
+                    }
+                }
+                else // if it's smaller then reset the bigCounter to 1
+                {
+                    bigCounter = 1;
+                }
+
+            }
+
+            Console.WriteLine(maxCounter); // return maxCounter after looping through the whole input array
+        }
+    }
+}
---
+++ 732A Buy a Shovel/732A Buy a Shovel/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _732A_Buy_a_Shovel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split(); // convert input to string
+            int k = int.Parse(input[0]); // out k - price of 1 shovel
+            int r = int.Parse(input[1]); // out r - coin different from 10
+
+            /* The idea is to calculate the price of n shovels (n=1,2,3...) until the last digit of the price is 0 or r. 10s, 100s and 1000s in the total price can be paid with infinite 10-coins so if the last digit is 0 or r, Polycarp can buy the given amount of shovels without change */
+
+            int lastDigit = k % 10; // get the last digit of original price 
+            int counter = 1; // set counter of shovels Polycarp can buy to 1
+            int temp = k; // declare temp price of all shovels that will increase by k (price of 1 shovel) with every loop iteration
+
+
+            while (lastDigit != 0 && lastDigit != r) // while last digit of k/temp isn't r (his exactly one coin of r burles) or 0 (infinite 10 coins) 
+            {
+                temp += k; // increase the temp by k to establish the price of n shovels
+                lastDigit = temp % 10; // calculate the last digit of the price
+                counter++; // increase the counter of shovels by 1
+            }
+
+            Console.WriteLine(counter); // return counter
+        }
+    }
+}
