d0dee4c374f8b043fbbaf4b67329763c1fc8722c
MattCobosco <mateuszkalucki@gmail.com>
2021-07-11 14:52:41 +0200

    228A, 266B

---
+++ 228A Is your horseshoe on the other hoof/228A Is your horseshoe on the other hoof/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace _228A_Is_your_horseshoe_on_the_other_hoof
+{
+    class Program
+    {
+        static void Main(string[] args) 
+
+            /* The idea is to count distinct values in the input and then subtract it from 4 (all horseshoes)*/
+        {
+            string[] colors = Console.ReadLine().Split(); // split input into string array
+
+            int distinctCount = colors.Distinct().Count(); // count distinct values in the array
+
+            Console.WriteLine(4 - distinctCount); // calculate and return result
+        }
+    }
+}
---
+++ 266B Queue at the School/266B Queue at the School/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _266B_Queue_at_the_School
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] nt = Console.ReadLine().Split(); // read input as 2d array of strings
+
+            int n = int.Parse(nt[0]); // out n (not used)
+            int t = int.Parse(nt[1]); // out t
+
+            string queue = Console.ReadLine(); // read queue as from input as string
+
+            while (t > 0) // repeat t times
+            {
+                queue = queue.Replace("BG", "GB"); // replace each BG in the string with GB (if a Boy is in front of a Girl, switch their places)
+                t--; // reduce t by 1 on each run
+            }
+            
+            Console.WriteLine(queue); // return new queue after t seconds
+        }
+    }
+}
