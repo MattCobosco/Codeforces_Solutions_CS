29e9b4b9d5909ff9c2a5cf946f1e3ce03db03cb0
MattCobosco <mateuszkalucki@gmail.com>
2021-07-12 23:47:22 +0200

    492A

---
+++ 492A Vanya and Cubes/492A Vanya and Cubes/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _492A_Vanya_and_Cubes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            int rowCounter = 0; // counter of rows built with cubes
+            int cubesUsed = 0; // counter of cubes used
+            int cubesInRow = 0; // number of cubes in each row
+            for (int i = 1; ; i++)
+            {
+                cubesInRow += i; // number of cubes in row is bigger by i with every loop
+                cubesUsed += cubesInRow; // number of cubes in row is added to the total counter of used cubes
+
+
+                if (cubesUsed > n) // if there are no cubes left, break the loop
+                    break;
+                else // else increase counter of rows built by 1
+                    rowCounter++;
+            }
+
+            Console.WriteLine(rowCounter); // return the counter of built rows after the loop is finished
+        }
+    }
+}
