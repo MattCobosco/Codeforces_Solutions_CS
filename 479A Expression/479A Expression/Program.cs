54f6a1cc9466b6a67ea31fa8d1c2c5f62f974eac
MattCobosco <mateuszkalucki@gmail.com>
2021-07-24 00:18:46 +0200

    1000 diff

---
+++ 479A Expression/479A Expression/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+
+namespace _479A_Eapression
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = Convert.ToInt16(Console.ReadLine()); // read a,b,c from input
+            int b = Convert.ToInt16(Console.ReadLine());
+            int c = Convert.ToInt16(Console.ReadLine());
+                        
+            int[] computations = new int[] // array with all possible results
+            {
+                a + b * c,
+                (a + b) * c,
+                a * b + c,
+                a * (b + c),
+                a + b + c,
+                a * b * c
+            };
+
+            Console.WriteLine(computations.Max()); // print the max value among the results
+        }
+    }
+}
